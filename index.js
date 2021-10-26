c8e535f08c9033917ef22e51e022e2add5f86c65
Tushar <tm2582001@gmail.com>
2021-10-26 17:08:14 +0530

    improved keyframe animation code using css variable
    this site now uses jquery waypoint for intersection observer

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 let id = (id) => document.getElementById(id);
+let classes = (classes) => document.getElementsByClassName(classes);
+let queryAll = (queryAll) => document.querySelectorAll(queryAll);
+
+let value =[99,95,96,94];
+let time = 0;
+
+for(let i =0;i<value.length;i++){
+    const circle = queryAll("svg circle")[i];
+    circle.style.setProperty('--stroke-dashoffset', 438-(438*(value[i]/100)));
+}
 
 function counter (number,maxPercentage){
     let counter = 0;
@@ -13,9 +23,24 @@ function counter (number,maxPercentage){
     },2000/maxPercentage);
 }
 
-let value =[99,95,96,94]
-
-for(let i= 1; i<=value.length;i++){
-   let number = id("number" + i);
-   counter(number,value[i-1]); 
+function skillPercentage(){
+    if(time===0){
+        for(let i= 0; i<value.length;i++){
+            let number = classes("number")[i];
+            counter(number,value[i]); 
+        }
+        time = 1;
+    }
 }
+
+var waypoint = new Waypoint({
+    element: id('skills'),
+    handler: function() {
+        for(let i =0;i<value.length;i++){
+            const circle = queryAll("svg circle")[i];
+            circle.style.setProperty('animation', "anim 2s linear forwards");
+        }
+        skillPercentage();
+    },
+    offset: 400
+  });
